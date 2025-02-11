13d4f5e696b75d0d0bfa9a1bd390f61d5567f811
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-11 00:25:11 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyAr92DVWZGaVmFOZq86Arqf0fm5dNiwmCA",
+            authDomain: "todo-80ms6j.firebaseapp.com",
+            projectId: "todo-80ms6j",
+            storageBucket: "todo-80ms6j.firebasestorage.app",
+            messagingSenderId: "45193345432",
+            appId: "1:45193345432:web:e56e9e261634435ea213ed"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
